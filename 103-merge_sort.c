1c8bda4bd75e2452357cf3641c671d46482e913f
saaid <saaid2538@gmail.com>
2023-07-25 04:30:05 -0700

    added 103-merge_sort.c, 103-O

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -13,33 +13,44 @@ void merge(int *array, int *left, int left_size, int *right, int right_size,
 		size_t size)
 {
 	int i = 0, j = 0, k = 0;
+	int *temp = malloc((left_size + right_size) * sizeof(int));
 
 	printf("Merging...\n");
 	printf("[left]: ");
 	print_array(left, left_size);
-	printf("[right: ");
+	printf("[right]: ");
 	print_array(right, right_size);
+	if (temp == NULL)
+	{
+		printf("Memory allocation failed.\n");
+		return;
+	}
 	while (i < left_size && j < right_size)
 	{
 		if (left[i] <= right[j])
 		{
-			array[k++] = left[i++];
+			temp[k++] = left[i++];
 		}
 		else
 		{
-			array[k++] = right[j++];
+			temp[k++] = right[j++];
 		}
 	}
 	while (i < left_size)
 	{
-		array[k++] = left[i++];
+		temp[k++] = left[i++];
 	}
 	while (j < right_size)
 	{
-		array[k++] = right[j++];
+		temp[k++] = right[j++];
 	}
-	printf("[Done]: ");
+	for (i = 0; i < left_size + right_size; i++)
+	{
+		array[i] = temp[i];
+	}
+	printf("Done: ");
 	print_array(array, size);
+	free(temp);
 }
 
 /**
@@ -51,32 +62,14 @@ void merge(int *array, int *left, int left_size, int *right, int right_size,
 void merge_sort(int *array, size_t size)
 {
 	int mid = size / 2;
-	int *left = (int *)malloc(mid * sizeof(int));
-	int *right = (int *)malloc((size - mid) * sizeof(int));
-	int i;
-	size_t j;
+	int *left = array;
+	int *right = array + mid;
 
 	if (size <= 1)
 	{
 		return;
 	}
-	if (left == NULL || right == NULL)
-	{
-		free(left);
-		free(right);
-		return;
-	}
-	for (i = 0; i < mid; i++)
-	{
-		left[i] = array[i];
-	}
-	for (j = mid; j < size; j++)
-	{
-		right[j - mid] = array[j];
-	}
 	merge_sort(left, mid);
 	merge_sort(right, size - mid);
 	merge(array, left, mid, right, size - mid, size);
-	free(left);
-	free(right);
 }
